ec738ace4df8fb7fd553202225a9183d2479d3bf
Aditya Chavan <127530500+cadityab09@users.noreply.github.com>
2025-01-20 12:13:37 +0530

    app.js error

--- src/App.js
+++ src/App.js
@@ -131,14 +131,14 @@ function App() {
             </>
           
         } />
-          <Route path="/DoctorLoginForm" element={<DoctorLoginForm />} /> 
+          {/* <Route path="/DoctorLoginForm" element={<DoctorLoginForm />} /> 
           <Route path="/DoctorDashboard/*" element={<Doctordashboard/>} /> 
           <Route path="/NurseLoginForm" element={<NurseLoginForm />} />
           <Route path="/Nursedashboard/*" element={<Nursedashboard/>} />
           <Route path="/AllPatient" element={<AllPatient/>} />
           <Route path="/add-patient" element={<AddPatient />} />
           <Route path="/create-appointment" element={<CreateAppointment />} />
-          <Route path="/add-enquiries" element={<AddEnquiries />} />     
+          <Route path="/add-enquiries" element={<AddEnquiries />} />      */}
         </Routes>
       </Router>
     <Router>
